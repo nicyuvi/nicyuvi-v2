243c0cc321767ba5aabe6f7f358fede5b1c1f64e
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-03-14 10:11:32 -0700

    style section header

--- src/app/(main)/(components)/experience.tsx
+++ src/app/(main)/(components)/experience.tsx
@@ -1,10 +1,21 @@
 export default function Experience() {
   return (
     <>
-      <h2>Experience</h2>
-      <div className="flex">
-        <div>left</div>
-        <div>right</div>
+      <h2 className="mx-auto max-w-sm border-b border-b-primary pb-2 text-center text-2xl">
+        Experience
+      </h2>
+      <div className="md:flex">
+        <div>2023 - Present</div>
+        <div>
+          <div>
+            <h2>Job Title</h2>
+            <i>link icon</i>
+          </div>
+          <span>Role Name</span>
+          <p>Job description.</p>
+          <span>tech one</span>
+          <span>tech two</span>
+        </div>
       </div>
     </>
   )
