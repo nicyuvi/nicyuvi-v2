c17b245710b5bb6085bd7fd16f04edb3d9f3e6f7
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-03-14 11:13:51 -0700

    style experience section for ipad views and up

--- src/app/(main)/(components)/experience.tsx
+++ src/app/(main)/(components)/experience.tsx
@@ -9,9 +9,11 @@ export default function Experience() {
       <h2 className="mx-auto mb-4 max-w-sm border-b border-b-primary pb-2 text-center text-2xl">
         Experience
       </h2>
-      <div className="md:flex">
-        <span className="text-muted-foreground">2023 - Present</span>
-        <div>
+      <div className="justify-center md:flex">
+        <span className="mr-4 w-1/2 text-right text-muted-foreground">
+          2023 - Present
+        </span>
+        <div className="w-1/2">
           <Link
             href="https://notehub-app-six.vercel.app/"
             className="flex items-center"
@@ -19,10 +21,10 @@ export default function Experience() {
             <h2 className="mr-2 text-2xl">Full-Stack Web Developer</h2>
             <ExternalLink />
           </Link>
-          <span className="mb-4 block text-xl text-muted-foreground">
+          <span className="mb-4 block text-muted-foreground">
             Indie Developer
           </span>
-          <p className="rounded bg-primary p-2">
+          <p className="mb-1 rounded bg-primary p-2">
             Working on Notehub: A collaborative, note taking web app
           </p>
           {techList.map((tech, index) => (
