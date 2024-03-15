e49a9491e4d985e754c115b813818a55943e90e9
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-03-15 09:13:22 -0700

    switch to grid layout. align date with company title

--- src/app/(main)/(components)/experience-card.tsx
+++ src/app/(main)/(components)/experience-card.tsx
@@ -21,18 +21,19 @@ export default function ExperienceCard({
       <h2 className="mx-auto mb-4 max-w-sm border-b border-b-primary pb-2 text-center text-2xl">
         Experience
       </h2>
-      <div className="justify-center sm:flex">
-        <span className="mr-4 text-right  text-muted-foreground sm:w-1/2">
+      <div className="items-end justify-center sm:grid sm:grid-cols-2">
+        <span className="mr-4 inline-block text-right align-baseline text-muted-foreground">
           {dates}
         </span>
-        <div className="sm:w-1/2">
-          <Link
-            href="https://notehub-app-six.vercel.app/"
-            className="flex items-center"
-          >
-            <h2 className="mr-2 text-2xl">{company}</h2>
-            <ExternalLink />
-          </Link>
+        <Link
+          href="https://notehub-app-six.vercel.app/"
+          className="flex items-center"
+        >
+          <h2 className="mr-2 text-2xl">{company}</h2>
+          <ExternalLink />
+        </Link>
+        <div />
+        <div className="">
           <span className="mb-4 block text-muted-foreground">{role}</span>
           <p className="mb-1 rounded bg-primary p-2">{description}</p>
           {techList.map((tech, index) => (
