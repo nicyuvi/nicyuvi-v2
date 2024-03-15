25d1161d60c2e132b5647e0cea8986cb65dd86a6
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-03-15 09:51:26 -0700

    update tech list and add links to experience

--- src/app/(main)/(components)/experience-card.tsx
+++ src/app/(main)/(components)/experience-card.tsx
@@ -7,6 +7,7 @@ type ExperienceCardProps = {
   role: string
   description: string
   techList: string[]
+  link?: string
 }
 
 export default function ExperienceCard({
@@ -15,29 +16,31 @@ export default function ExperienceCard({
   role,
   description,
   techList,
+  link,
 }: ExperienceCardProps) {
   return (
     <>
-      <h2 className="mx-auto mb-4 max-w-sm border-b border-b-primary pb-2 text-center text-2xl">
-        Experience
-      </h2>
       <div className="items-end justify-center sm:grid sm:grid-cols-2">
         <span className="mr-4 inline-block text-right align-baseline text-muted-foreground">
           {dates}
         </span>
         <Link
-          href="https://notehub-app-six.vercel.app/"
+          href={link || '/'}
+          target={link ? '_blank' : '_self'}
           className="flex items-center"
         >
           <h2 className="mr-2 text-2xl">{company}</h2>
           <ExternalLink />
         </Link>
         <div />
-        <div className="">
+        <div>
           <span className="mb-4 block text-muted-foreground">{role}</span>
           <p className="mb-1 rounded bg-primary p-2">{description}</p>
           {techList.map((tech, index) => (
-            <span key={index} className="mr-2 text-muted-foreground">
+            <span
+              key={index}
+              className="mr-2 inline-block text-muted-foreground"
+            >
               {tech}
             </span>
           ))}
--- src/app/(main)/(components)/experience.tsx
+++ src/app/(main)/(components)/experience.tsx
@@ -6,38 +6,67 @@ export default function Experience() {
     company: 'Indie Developer',
     role: 'Full-Stack Web Developer',
     description: 'Working on Notehub: A collaborative, note taking web app',
-    techList: ['NodeJS', 'Tailwind CSS', 'Prisma', 'Python', 'Flask'],
+    techList: [
+      'NextJS',
+      'TypeScript',
+      'NodeJS',
+      'TailwindCSS',
+      'Prisma',
+      'Postgres',
+      'SQLite',
+      'Python',
+      'Flask',
+    ],
   }
 
   const lensabl = {
     dates: '2021 - 2023',
     company: 'Lensabl',
-    role: 'Front-End Web Developer',
+    role: 'Full-Stack Web Developer',
     description: `
       Built a custom ecommerce website and a dashboard that allows clients to customize the website. 
       Successfully demoed TailwindCSS to my team and implemented a scalable CSS solution for two SaaS projects.
       Onboarded and mentored a new developer intern to the team.
       Built an API to sanitize form data, send the data to an email, and send a response back to the client.
     `,
-    techList: ['NodeJS', 'Tailwind CSS', 'Prisma', 'Python', 'Flask'],
+    techList: [
+      'NextJS',
+      'ReactJS',
+      'HTML/SCSS',
+      'TailwindCSS',
+      'TanStack Query',
+      'React Router',
+      'Redux',
+      'NodeJS',
+      'PHP',
+    ],
+    link: 'https://lensabl.com',
   }
 
   return (
     <>
-      <ExperienceCard
-        dates={curr.dates}
-        company={curr.company}
-        role={curr.role}
-        description={curr.description}
-        techList={curr.techList}
-      />
-      <ExperienceCard
-        dates={lensabl.dates}
-        company={lensabl.company}
-        role={lensabl.role}
-        description={lensabl.description}
-        techList={lensabl.techList}
-      />
+      <h2 className="mx-auto mb-4 max-w-sm border-b border-b-primary pb-2 text-center text-2xl">
+        Experience
+      </h2>
+      <div className="mb-6">
+        <ExperienceCard
+          dates={curr.dates}
+          company={curr.company}
+          role={curr.role}
+          description={curr.description}
+          techList={curr.techList}
+        />
+      </div>
+      <div className="mb-6">
+        <ExperienceCard
+          dates={lensabl.dates}
+          company={lensabl.company}
+          role={lensabl.role}
+          description={lensabl.description}
+          techList={lensabl.techList}
+          link={lensabl.link}
+        />
+      </div>
     </>
   )
 }
