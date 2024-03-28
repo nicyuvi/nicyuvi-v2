85701c9378bca610b8ab484addaa2233dd77db1d
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-03-28 14:30:47 -0700

    install react simple icons and include brand icons in side nav

--- src/components/layout/side-nav.tsx
+++ src/components/layout/side-nav.tsx
@@ -1,10 +1,26 @@
+import {
+  SiGithub,
+  SiLinkedin,
+  SiCodepen,
+  SiTwitter,
+} from '@icons-pack/react-simple-icons'
+
 export default function SideNav() {
   return (
     <nav className="fixed right-0 top-1/2">
       <ul>
-        <li>one</li>
-        <li>two</li>
-        <li>three</li>
+        <li>
+          <SiGithub />
+        </li>
+        <li>
+          <SiLinkedin />
+        </li>
+        <li>
+          <SiCodepen />
+        </li>
+        <li>
+          <SiTwitter />
+        </li>
       </ul>
     </nav>
   )
