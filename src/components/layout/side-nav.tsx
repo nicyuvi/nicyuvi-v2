303890a1bf14fd4d7b2df58bb93b7be8c3c50b72
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-03-28 14:55:35 -0700

    add social icon and links

--- src/components/layout/side-nav.tsx
+++ src/components/layout/side-nav.tsx
@@ -6,24 +6,44 @@ import {
 } from '@icons-pack/react-simple-icons'
 import Link from 'next/link'
 
+type SocialIcon = {
+  link: string
+  icon: React.ReactNode
+}
+
+const socialIcons: SocialIcon[] = [
+  {
+    link: 'https://github.com/nicyuvi',
+    icon: <SiGithub />,
+  },
+  {
+    link: 'https://linkedin.com/in/nicyuvi',
+    icon: <SiLinkedin />,
+  },
+  {
+    link: 'https://codepen.io/nicyuvi/pens/',
+    icon: <SiCodepen />,
+  },
+  {
+    link: 'https://twitter.com/nicyuvi',
+    icon: <SiTwitter />,
+  },
+]
+
 export default function SideNav() {
   return (
     <nav className="fixed right-4 top-1/2">
       <ul className="flex flex-col gap-4">
-        <li className="cursor-pointer transition duration-300 hover:-translate-y-1 hover:text-primary">
-          <Link href="https://github.com/nicyuvi" target="_blank">
-            <SiGithub />
-          </Link>
-        </li>
-        <li>
-          <SiLinkedin />
-        </li>
-        <li>
-          <SiCodepen />
-        </li>
-        <li>
-          <SiTwitter />
-        </li>
+        {socialIcons.map((socialIcon, index) => (
+          <li
+            key={index}
+            className="cursor-pointer transition duration-300 hover:-translate-y-1 hover:text-primary"
+          >
+            <Link href={socialIcon.link} target="_blank">
+              {socialIcon.icon}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   )
