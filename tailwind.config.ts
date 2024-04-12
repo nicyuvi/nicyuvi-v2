745d15489221dfea5b67b9feb9f8a656e3e561c3
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-04-12 10:33:37 -0700

    add default screens to tailwind config for readability

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,14 +10,16 @@ const config = {
   ],
   prefix: '',
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     container: {
       center: true,
       padding: '3rem',
-      screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-      },
     },
     extend: {
       colors: {
