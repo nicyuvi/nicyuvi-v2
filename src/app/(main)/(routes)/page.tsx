0d1ba78072f1696dbb3c861632c9e901797b8b6e
Nicolis Yuvienco <nickyuvienco@gmail.com>
2025-01-14 13:35:28 -0800

    Revert "redesign hero section"
    This reverts commit 3e6f1379e1f49849efc9b8309659a3a53f7fafbb.

--- src/app/(main)/(routes)/page.tsx
+++ src/app/(main)/(routes)/page.tsx
@@ -1,25 +1,20 @@
 import Contact from './(components)/contact'
 import Experience from './(components)/experience'
 import Hero from './(components)/hero'
-// import Model from './(components)/model'
+import Model from './(components)/model'
 import Projects from './(components)/projects'
 
 export default function Home() {
   return (
     <>
-      <section className="flex h-[calc(100vh_-_4rem)] items-center">
-        <Hero />
+      <section className="flex h-[calc(100vh_-_4rem)] min-h-0 flex-col items-center md:flex-row">
+        <div className="md:w-3/5">
+          <Hero />
+        </div>
+        <div className="w-full md:h-[calc(100vh_-_4rem)] md:w-2/5">
+          <Model />
+        </div>
       </section>
-      {
-        // <section className="flex h-[calc(100vh_-_4rem)] min-h-0 flex-col items-center md:flex-row">
-        // <div className="md:w-3/5">
-        //   <Hero />
-        // </div>
-        // <div className="w-full md:h-[calc(100vh_-_4rem)] md:w-2/5">
-        //   <Model />
-        // </div>
-        // </section>
-      }
       <section id="experience" className="pt-20">
         <Experience />
       </section>
